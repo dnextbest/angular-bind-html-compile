d02d6011784e6b4a73ed78d1b6a429873af34caf
Dominik Szymanski <dominosz89@gmail.com>
2015-05-29 23:45:43 +0200

    Ensure that compilation will fire once
    It will avoid unnecessary scope iterations.

--- angular-bind-html-compile.js
+++ angular-bind-html-compile.js
@@ -7,11 +7,12 @@
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
-                scope.$watch(function () {
+                var runOnce = scope.$watch(function () {
                     return scope.$eval(attrs.bindHtmlCompile);
                 }, function (value) {
                     element.html(value);
                     $compile(element.contents())(scope);
+                    runOnce();
                 });
             }
         };
